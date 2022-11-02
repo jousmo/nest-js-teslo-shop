1faae42ff8cb102d76a1dc06fcf303a1659e30d4
José Uscanga <jousmo@outlook.com>
2022-11-01 22:11:31 -0600

    feat: include new env vars en joi and env config

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,18 @@
 import { registerAs } from '@nestjs/config';
 
+const environment = process.env.NODE_ENV || 'dev';
+
 export default registerAs('config', () => ({
-  environment: process.env.NODE_ENV || 'dev',
+  environment,
   port: +process.env.PORT,
+  database: {
+    type: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: +process.env.DB_PORT,
+    synchronize: environment !== 'production',
+    autoLoadEntities: environment !== 'production',
+  },
 }));
--- src/config/joi.config.ts
+++ src/config/joi.config.ts
@@ -3,4 +3,10 @@ import * as Joi from 'joi';
 export const JoiConfig = Joi.object({
   NODE_ENV: Joi.string().required().default('dev'),
   PORT: Joi.number().required(),
+  DB_DIALECT: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
+  DB_USER: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_PORT: Joi.number().required(),
 });
