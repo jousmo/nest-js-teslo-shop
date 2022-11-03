ee1c67dccc320e175ee9001d6d09c75a70d0ecca
jose.uscanga <jousmo@outlook.com>
2022-11-03 15:12:05 -0600

    feat: implement create product, with context logger Product Service

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -10,13 +10,14 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
+  create(@Body() createProductDto: CreateProductDto): Promise<Product> {
     return this.productsService.create(createProductDto);
   }
 
