7f1fab770dcad2a331f7a96a7f4633211e580459
jose.uscanga <jousmo@outlook.com>
2022-11-03 15:11:24 -0600

    feat: create dto for products

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,41 @@
-export class CreateProductDto {}
+import {
+  IsString,
+  MinLength,
+  IsInt,
+  IsPositive,
+  IsOptional,
+  IsArray,
+  IsEnum,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  price: number;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @IsString()
+  @MinLength(1)
+  slug: string;
+
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  stock: number;
+
+  @IsArray()
+  @IsString({ each: true })
+  sizes: string[];
+
+  @IsString()
+  @IsEnum(['men', 'women', 'unisex', 'kid'])
+  gender: string;
+}
