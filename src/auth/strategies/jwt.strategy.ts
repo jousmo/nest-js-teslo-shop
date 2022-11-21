5e84bbcc12ff4488e36fd8136785d0b5f74a134b
José Uscanga <jousmo@outlook.com>
2022-11-21 15:12:07 -0600

    feat: implement jwt strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Inject, Injectable } from '@nestjs/common';
+import config from '../../config/env.config';
+import { ConfigType } from '@nestjs/config';
+import { User } from '../entities/user.entity';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @Inject(config.KEY)
+    private readonly configService: ConfigType<typeof config>,
+    private readonly authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.jwtSecret,
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { email } = payload;
+    return await this.authService.checkIfUserExistAndIsActive(email);
+  }
+}
