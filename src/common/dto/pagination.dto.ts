39e84d9b0b7d069b85645cab06dcde994c8a48e8
jose.uscanga <jousmo@outlook.com>
2022-11-22 16:08:26 -0600

    feat: more documentation examples

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,12 +1,21 @@
 import { IsOptional, Min, IsInt } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PaginationDto {
+  @ApiProperty({
+    default: 10,
+    description: 'How many rows do you need',
+  })
   @IsOptional()
   @IsInt()
   @Type(() => Number) // equal => enableImplicitConversion
   limit?: number;
 
+  @ApiProperty({
+    default: 0,
+    description: 'How many rows do you want to skip',
+  })
   @IsOptional()
   @IsInt()
   @Min(0)
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -7,44 +7,58 @@ import {
   IsArray,
   IsEnum,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product title (unique)',
+    nullable: false,
+    minLength: 1,
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsInt()
   @IsPositive()
   price: number;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   description: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   @MinLength(1)
   slug: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsInt()
   @IsPositive()
   stock: number;
 
+  @ApiProperty()
   @IsArray()
   @IsString({ each: true })
   sizes: string[];
 
+  @ApiProperty()
   @IsString()
   @IsEnum(['men', 'women', 'unisex', 'kid'])
   gender: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   tags: string[];
 
+  @ApiProperty()
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
