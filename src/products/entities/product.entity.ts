c1882d77e2484cedef7c71d86305cb68c4a6093f
José Uscanga <jousmo@outlook.com>
2022-11-21 22:19:32 -0600

    feat: create relationship user products

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,10 +3,12 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { ProductImage } from './product-image.entity';
+import { User } from '../../auth/entities/user.entity';
 
 @Entity({ name: 'products' })
 export class Product {
@@ -43,6 +45,10 @@ export class Product {
     eager: true, // Load all relationship with use find*
   })
   images?: ProductImage[];
+
+  // Many products have one user
+  @ManyToOne(() => User, (user) => user.products, { eager: true })
+  user: User;
 
   @BeforeInsert()
   checkSlugInsert() {
