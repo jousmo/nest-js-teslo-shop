0b17000bd12447698edd60b30779f97e02a90010
José Uscanga <jousmo@outlook.com>
2022-11-21 21:09:50 -0600

    feat: implement custom decorators for get users and get headers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,20 @@
-import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { Express } from 'express';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Headers,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities/user.entity';
+import { RawHeaders } from '../common/decorators/raw-headers.decorator';
+import { IncomingHttpHeaders } from 'http';
 
 @Controller('auth')
 export class AuthController {
@@ -20,7 +32,12 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  testPrivateRoute() {
-    return { message: 'private route' };
+  testPrivateRoute(
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @RawHeaders() rawHeaders: Express.Request,
+    @Headers() headers: IncomingHttpHeaders,
+  ) {
+    return { user, message: 'Private route', userEmail, rawHeaders, headers };
   }
 }
