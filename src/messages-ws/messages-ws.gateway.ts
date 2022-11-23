c028b43c9c439b68c78b46abbd2d335aed73ab64
José Uscanga <jousmo@outlook.com>
2022-11-23 07:41:58 -0600

    feat: implement socket basic

---
+++ src/messages-ws/messages-ws.gateway.ts
@@ -0,0 +1,22 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { MessagesWsService } from './messages-ws.service';
+
+@WebSocketGateway({ cors: true })
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(private readonly messagesWsService: MessagesWsService) {}
+
+  handleConnection(client: Socket): any {
+    console.log('Client connected:', client.id);
+  }
+
+  handleDisconnect(client: Socket): any {
+    console.log('Client disconnect:', client.id);
+  }
+}
